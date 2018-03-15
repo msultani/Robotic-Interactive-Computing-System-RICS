660b8e94f448dc852712b7a079db43e3a7a20824
Mark Sultani <msultani@umich.edu>
2018-03-15 18:59:15 -0400

    speech api cloud

--- speech_recognition/speech_recognition/__main__.py
+++ speech_recognition/speech_recognition/__main__.py
@@ -14,7 +14,8 @@ commands = ["wake",
             "left", # move?
             "right", # move?
             "forward", # move?
-            "backward"] # move?
+            "backward" # move?
+            ]
 
 
 def process_text(text):
@@ -28,6 +29,18 @@ def process_text(text):
             keep.append(word)
     return keep
 
+
+# See python_ports.py from kccla@umich.edu
+def send_message(command_value):
+    sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sender.connect(("0.0.0.0", 6000))
+    data = json.dumps( {"message_value" : command_value} )
+
+    sender.sendall(str.encode(data))
+    sender.close()
+
+
+
 try:
     print("A moment of silence, please...")
     with m as source: r.adjust_for_ambient_noise(source)
@@ -37,8 +50,11 @@ try:
         with m as source: audio = r.listen(source)
         print("Got it! Now to recognize it...")
         try:
-            # recognize speech using Google Speech Recognition
-            value = r.recognize_google(audio)
+            with open("RICS-1cde6e998337.json") as json_data:
+                d = json.load(json_data)
+                # print(d)
+                # recognize speech using Google Speech Recognition
+                value = r.recognize_google_cloud(audio, credentials_json=json.dumps(d), language="en-US", preferred_phrases=commands, show_all=False)
 
             # TODO RICS: process and send value
             send = socket.socket()
